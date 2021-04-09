51b96a16725f96b210f5341d6ccd839a5b241f55
Ahmedsamhar <ahmed.samhar@student.guc.edu.eg>
2021-04-09 19:24:13 +0200

    Create interaction usp

---
+++ uspCreateInteraction.sql
@@ -0,0 +1,15 @@
+CREATE or replace procedure uspCreateInteraction ( 
+	source_user_id int, 
+	target_user_id int, 
+	"type" interaction_type
+)
+language plpgsql
+	
+AS $$
+	begin
+
+		insert into interactions(source_user_id,target_user_id,"type",created_at) 
+		values (@source_user_id,@target_user_id,@"type",CURRENT_TIMESTAMP);		
+	
+	commit;
+	end; $$
