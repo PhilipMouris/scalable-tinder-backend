13f9abf86f3beb46d0f25b9488e7c8e70e49a259
YoussefFathi <youssef.fathy1164@gmail.com>
2021-04-09 14:52:28 +0200

    Add-Tests

---
+++ db/storedProcedures/blocks_bans_transactions/tests.sql
@@ -0,0 +1,19 @@
+-- SELECT * FROM "uspReadAllBans"(1);
+
+-- CALL "uspDeleteBan"(1,2);
+
+-- DROP FUNCTION "uspReadBans"(integer)
+
+-- SELECT * FROM "uspCreateBan"(1,3,'IHAM',current_date);
+
+-- SELECT * FROM "uspUpdateBan"(2,1,2,'ALO2',NULL);
+
+-- SELECT * FROM "uspDeleteBan"(2);
+
+-- SELECT * FROM "uspUpdateBlock"(1,2,NULL);
+
+-- SELECT * FROM "uspReadAllBlocks"(0,20);
+
+-- SELECT * FROM "uspDeleteBlock"(1,2);
+
+-- SELECT * FROM "uspUpdateTransaction"(1,1,50000::MONEY,NULL);
