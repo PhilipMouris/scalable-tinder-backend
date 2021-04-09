be9bb9ffa69ce149d800a4f26cb57e0c830f349f
Ahmedsamhar <ahmed.samhar@student.guc.edu.eg>
2021-04-10 00:41:23 +0200

    remove current_time & returning interaction created

--- uspCreateInteraction.sql
+++ uspCreateInteraction.sql
@@ -1,3 +1,13 @@
+CREATE TYPE "interactionData" As(
+	id int,
+	source_user_id int,
+	target_user_id int,
+	"type" interaction_type,
+	created_at timestamp
+)
+	
+
+
 CREATE or replace procedure uspCreateInteraction ( 
 	source_user_id int, 
 	target_user_id int, 
@@ -6,10 +16,17 @@ CREATE or replace procedure uspCreateInteraction (
 language plpgsql
 	
 AS $$
+	declare
+		interaction_data "interactionData";	
+	
 	begin
 
-		insert into public.interactions(source_user_id,target_user_id,"type",created_at) 
-		values (source_user_id,target_user_id,"type",CURRENT_TIMESTAMP);		
+		insert into public.interactions(source_user_id,target_user_id,"type") 
+		values (source_user_id,target_user_id,"type");		
+	
+
+	returning * into interaction_data;
+	return interaction_data;
 	
 	commit;
 	end; $$
