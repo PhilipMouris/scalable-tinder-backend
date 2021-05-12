625fa1645509992ca339026059b81d17f51ef8d8
Alsouidan <alsouidan@gmail.com>
2021-05-12 22:29:11 +0200

    Adding create interaction

--- Backend/src/main/java/Commands/UserToUserCommands/CreateInteraction.java
+++ Backend/src/main/java/Commands/UserToUserCommands/CreateInteraction.java
@@ -6,7 +6,7 @@ import Interface.ConcreteCommand;
 public class CreateInteraction extends ConcreteCommand {
     @Override
     public void setParameters() {
-        storedProcedure = "\"uspCreateInteraction\"";
+        storedProcedure = "uspCreateInteraction";
         inputParams = new String[]{"interactionData.source_user_id", "interactionData.target_user_id", "interactionData.type"};
         outputName = "interaction";
     }
