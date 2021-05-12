7ea382daa7b1456fae83eadfd611a9f01c4fbb6d
Alsouidan <alsouidan@gmail.com>
2021-05-12 22:55:17 +0200

    Adding Arango Max connections in service classes

--- Backend/src/main/java/Services/ChatService.java
+++ Backend/src/main/java/Services/ChatService.java
@@ -48,6 +48,7 @@ public class ChatService extends ServiceControl{
 
     @Override
     public boolean setMaxDBConnections(String connections){
+        arangoInstance.setMaxDBConnections(Integer.parseInt(connections));
         return postgresDB.setDbMaxConnections(connections+"");
 //        ChatArangoInstance.setMaxDBConnections(maxDBConnections);
     }
